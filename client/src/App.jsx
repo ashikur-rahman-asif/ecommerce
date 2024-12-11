de7769246637a95dc7f5cccc4d10c6b8590386ca
asif247 <ashikurrahmanasif247@yahoo.com>
2024-12-11 20:01:41 +0600

    shadcn setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl text-red-500">This is the ecom site</h1>
+      <Button>Asif</Button>
     </>
   );
 }
