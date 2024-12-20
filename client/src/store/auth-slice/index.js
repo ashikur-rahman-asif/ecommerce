f34c3d6393fa98782bcb7979360cd429dd9fe367
asif247 <ashikurrahmanasif247@yahoo.com>
2024-12-20 23:39:48 +0600

    store setup done

---
+++ client/src/store/auth-slice/index.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    isAuthenticated:false,
+    isLoading: false,
+    user:null,
+    isError: false,
+    error: null,
+}
+const authSlice=createSlice({
+    name:"auth",
+    initialState,
+    reducers:{
+        setUser:(state,action)=>{
+
+        }
+    }
+})
+
+export const {}=authSlice.actions
+export default authSlice.reducer
