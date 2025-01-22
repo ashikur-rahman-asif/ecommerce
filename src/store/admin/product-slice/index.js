f58344ddad87d79030eddd86946bd309fb9627fa
asif247 <ashikurrahmanasif247@yahoo.com>
2025-01-22 20:48:24 +0600

    add a  new product asyncThunk done

---
+++ src/store/admin/product-slice/index.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  productList: [],
+};
+
+// add a new product 
+export const addNewProduct = createAsyncThunk(
+  "/product/addnewproduct",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/admin/products/add",
+        formData,
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      return response.data; // Return response data to the reducer
+    } catch (error) {
+      // Handle errors and return the error message
+      return rejectWithValue(error.response?.data || "Failed to add product");
+    }
+  }
+);
+const AdminProductSlice = createSlice({
+  name: "adminProductSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {},
+});
+
+export default AdminProductSlice;
