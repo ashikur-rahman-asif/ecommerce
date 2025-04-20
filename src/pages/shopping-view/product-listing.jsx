0c125f19b85831f84ee5c7fe3fb819a8a40c2e57
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-21 02:56:14 +0600

    some basic config and filter btn done

--- src/pages/shopping-view/product-listing.jsx
+++ src/pages/shopping-view/product-listing.jsx
@@ -1,9 +1,47 @@
 import ProductFilter from "@/components/admin-view/filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { sortOptions } from "@/config";
+import { ArrowUpDown } from "lucide-react";
 
 const ShoppingListing = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter />
+      <div className="bg-background w-full rounded-lg shadow-sm">
+        <div className="p-3 gap-3 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold mr-2">All products</h2>
+          <div className="flex items-center gap-2">
+            <span className="text-muted-foreground">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1">
+                  <ArrowUpDown size="4" />
+                  <span>Sort By</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                 {
+                  sortOptions.map(item=><DropdownMenuRadioItem key={item.id}>
+                    {item.label}
+                  </DropdownMenuRadioItem>)
+                 }
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
