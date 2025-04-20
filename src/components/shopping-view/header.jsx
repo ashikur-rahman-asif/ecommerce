1d5738f0320323689e83695920a772b006796a97
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-21 00:23:03 +0600

    header menu and logout implement

--- src/components/shopping-view/header.jsx
+++ src/components/shopping-view/header.jsx
@@ -1,8 +1,113 @@
-const ShoppingHeader = () => {
+import { shoppingViewHeaderMenuItems } from "@/config";
+import { logoutUser } from "@/store/auth-slice";
+import { HousePlug, LogOut, Menu, ShoppingCart, UserCog } from "lucide-react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router";
+import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Label } from "../ui/label";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+
+function MenuItems() {
   return (
-    <div>
-      <h1>ShoppingHeader</h1>
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Label
+          onClick={() => handleNavigate(menuItem)}
+          className="text-sm font-medium cursor-pointer"
+          key={menuItem.id}>
+          {menuItem.label}
+        </Label>
+      ))}
+    </nav>
+  );
+}
+
+function HeaderRightContent() {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    navigate("/shop/account");
+    console.log("clicked");
+  };
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Sheet>
+        <Button variant="outline" size="icon" className="relative">
+          <ShoppingCart className="w-6 h-6" />
+          <span className="sr-only">User cart</span>
+        </Button>
+      </Sheet>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56 mt-16">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <UserCog className="mr-2 h-4 w-4" />
+            Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
+  );
+}
+
+const ShoppingHeader = () => {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+  console.log(isAuthenticated, user);
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
   );
 };
 
