ccd02a22f59fe3b626597c9d7678a8a9a6c961cc
asif247 <ashikurrahmanasif247@yahoo.com>
2025-04-28 17:44:35 +0600

    single product fetching store  code done

--- src/store/shop/products-slice/index.js
+++ src/store/shop/products-slice/index.js
@@ -4,6 +4,7 @@ import axios from "axios";
 const initialState = {
   isLoading: false,
   productList: [],
+  productDetails: null,
 };
 
 export const fetchAllFilteredProducts = createAsyncThunk(
@@ -22,6 +23,18 @@ export const fetchAllFilteredProducts = createAsyncThunk(
   }
 );
 
+// fetch single product
+export const fetchSingleProduct = createAsyncThunk(
+  "/products/fetchSingleProduct",
+  async (id) => {
+    const result = await axios.get(
+      `http://localhost:3000/api/shop/products/get/${id}`
+    );
+
+    return result?.data;
+  }
+);
+
 const ShopProductsSlice = createSlice({
   name: "shopProductSlice",
   initialState,
@@ -31,6 +44,15 @@ const ShopProductsSlice = createSlice({
       .addCase(fetchAllFilteredProducts.pending, (state) => {
         state.isLoading = true;
       })
+      .addCase(fetchSingleProduct.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchSingleProduct.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.isLoading = false;
+        state.productDetails = action.payload?.data || [];
+      })
+
       .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
         console.log(action.payload);
         state.isLoading = false;
@@ -39,6 +61,11 @@ const ShopProductsSlice = createSlice({
       .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
         state.isLoading = false;
         state.productList = [];
+        console.error("Error fetching products:", action.payload);
+      })
+      .addCase(fetchSingleProduct.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = [];
         console.error("Error fetching products:", action.payload);
       });
   },
